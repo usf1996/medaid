6ac2a029a6da76841bb063af169adf0b503ec7c1
Youssef El Khoury <usf.kh96@gmail.com>
2017-04-29 14:16:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -29,12 +29,12 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
 
 $tsql= "SELECT * FROM test";
 $getResults= sqlsrv_query($conn, $tsql);
-echo ("Reading data from table test" . PHP_EOL);
+echo ("Reading data from table dbo.test" . PHP_EOL . "<br>");
 
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    echo ($row['id'] . PHP_EOL);
+    echo ($row['id'] . PHP_EOL . "<br>");
 }
 
 sqlsrv_free_stmt($getResults);
