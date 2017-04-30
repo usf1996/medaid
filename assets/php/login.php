3a26052d0339680c7201059544ae291906dd3e34
Youssef El Khoury <usf.kh96@gmail.com>
2017-04-30 11:04:56 +0300

    login

--- assets/php/login.php
+++ assets/php/login.php
@@ -1,8 +1,23 @@
 <html>
 <head>
-</head>
-<body>
 <?php
+
+$serverName = "medaid.database.windows.net";
+$connectionOptions = array(
+    "Database" => "medaid",
+    "Uid" => "medaid",
+    "PWD" => "Test1234"
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+?>
+</head>
+
+<body>
+<h1>PHP connect to Azure</h1>
+ 
+<?php
+
 $serverName = "medaid.database.windows.net";
 $connectionOptions = array(
     "Database" => "medaid",
@@ -12,27 +27,19 @@ $connectionOptions = array(
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-if($conn)
-	echo "Connection Successfull";
-else
-	echo "Connection Not Successfull"
-
-$email = $_POST['email'];
-$password  = $_POST['password'];
-echo "$email" . " $password";
-
-$tsql= "SELECT * FROM users";
-
+$tsql= "SELECT * FROM test";
 $getResults= sqlsrv_query($conn, $tsql);
+echo ("Reading data from table dbo.test" . PHP_EOL . "<br>");
 
 if ($getResults == FALSE)
-    echo "nayyak";
-
-while ($row = sqlsrv_fetch_array($getResults)) {
-    echo "sex";
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    echo ($row['id'] . PHP_EOL . "<br>");
 }
-sqlsrv_free_stmt($getResults);
-?>
 
+sqlsrv_free_stmt($getResults);
+
+?>
+ 
 </body>
 </html>
