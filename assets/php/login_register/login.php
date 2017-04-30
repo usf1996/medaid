5645c0843aec7f0755708509bb713f47da5ea5d6
Youssef El Khoury <usf.kh96@gmail.com>
2017-04-30 23:11:44 +0300

    login

--- assets/php/login_register/login.php
+++ assets/php/login_register/login.php
@@ -4,7 +4,12 @@ include 'connect.php';
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$tsql= "SELECT * FROM citizen WHERE email = '$email' AND pass = '$password'";
+$tsql= "SELECT * FROM (
+		SELECT email, pass, usertype FROM citizen
+		UNION
+		SELECT email, pass, usertype FROM donationcenter
+		UNION
+		SELECT email, pass, usertype FROM redcross) AS loginres WHERE email = '$email' AND pass = '$password'";
 $getResults= sqlsrv_query($conn, $tsql);
 
 if ($getResults == FALSE)
@@ -13,21 +18,20 @@ else{
 	if(!sqlsrv_has_rows($getResults)){
 		$usertype = 0;
 	}else{
-		while ($row = sqlsrv_fetch_array($getResults)) {
-			if($email == $row['email'] && $password == $row['pass']){
-				switch($row['usertype']){
-					case "citizen":{
-						$usertype = 1;
-						break;
-					}
-					case "donation center":{
-						$usertype = 2;
-						break;
-					}
-					case "red cross":{
-						$usertype = 3;
-						break;
-					}
+		$row = sqlsrv_fetch_array($getResults)
+		if($email == $row['email'] && $password == $row['pass']){
+			switch($row['usertype']){
+				case "citizen":{
+					$usertype = 1;
+					break;
+				}
+				case "donation center":{
+					$usertype = 2;
+					break;
+				}
+				case "red cross":{
+					$usertype = 3;
+					break;
 				}
 			}
 		}
