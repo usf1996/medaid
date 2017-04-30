a1a715ebfce9116b4fbe2b133aa69c0f95ba33fb
Youssef El Khoury <usf.kh96@gmail.com>
2017-04-30 11:08:57 +0300

    login

--- assets/php/login.php
+++ assets/php/login.php
@@ -18,23 +18,14 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
  
 <?php
 
-$serverName = "medaid.database.windows.net";
-$connectionOptions = array(
-    "Database" => "medaid",
-    "Uid" => "medaid",
-    "PWD" => "Test1234"
-);
-//Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-
-$tsql= "SELECT * FROM test";
+$tsql= "SELECT * FROM users";
 $getResults= sqlsrv_query($conn, $tsql);
-echo ("Reading data from table dbo.test" . PHP_EOL . "<br>");
+echo ("Reading data from table dbo.users" . PHP_EOL . "<br>");
 
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    echo ($row['id'] . PHP_EOL . "<br>");
+    echo ($row['username'] . PHP_EOL . "<br>");
 }
 
 sqlsrv_free_stmt($getResults);
