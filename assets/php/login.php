a409027f817e5f1269e6887593fd738f54430287
Youssef El Khoury <usf.kh96@gmail.com>
2017-04-30 10:49:24 +0300

    login

--- assets/php/login.php
+++ assets/php/login.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 $password  = $_POST['password'];
 echo "$email" . " $password";
 
-$tsql= "SELECT email, pass FROM users WHERE email = '$email' AND pass = '$password'";
+$tsql= "SELECT email, pass FROM users WHERE email = "$email" AND pass = "$password"";
 
 $getResults= sqlsrv_query($conn, $tsql);
 
@@ -13,7 +13,7 @@ if ($getResults == FALSE)
     echo (sqlsrv_errors());
 
 while ($row = sqlsrv_fetch_array($getResults)) {
-    echo ($row['email'] . PHP_EOL);
+    echo "sex";
 }
 sqlsrv_free_stmt($getResults);
 ?>
