5d3db836e0c5ce690723f6d1d4a4baf15100aa8a
Youssef El Khoury <usf.kh96@gmail.com>
2017-04-30 12:20:25 +0300

    login

--- assets/php/login.php
+++ assets/php/login.php
@@ -10,12 +10,12 @@ $getResults= sqlsrv_query($conn, $tsql);
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 else{
-	if(sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC) == 0){
+	$row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC);
+	if($row == 0){
 		$usertype = 0;
 		echo 0;
 	}else{
-		echo sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC);
-		while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+		while ($row) {
 			if($email == $row['fname'] && $password == $row['pass']){
 				switch($row['usertype']){
 					case "citizen":{
