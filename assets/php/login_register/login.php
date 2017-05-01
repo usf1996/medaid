9082daa5b52431edfee1bd88d7b1eb137d358373
Youssef El Khoury <usf.kh96@gmail.com>
2017-05-01 19:13:58 +0300

    pass data

--- assets/php/login_register/login.php
+++ assets/php/login_register/login.php
@@ -21,7 +21,7 @@ else{
 		$data['usertype'] = 0;
 	}else{
 		while ($row = sqlsrv_fetch_array($getResults)) {
-					$data['dcenterid'] = $row['email'];
+					$data['dcenterid'] = $row['usertype'];
 			switch($row['usertype']){
 				case "citizen":{
 					$data['usertype'] = 1;
