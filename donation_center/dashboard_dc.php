645a0c63b7c46280c16bf7866d6ed3d3519dcfe5
Youssef El Khoury <usf.kh96@gmail.com>
2017-05-01 18:31:47 +0300

    pass data

--- donation_center/dashboard_dc.php
+++ donation_center/dashboard_dc.php
@@ -261,6 +261,7 @@
 			xhttp=new XMLHttpRequest();
 			xhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
+					console.log("fet");
 					cFunction(this);
 				}
 			};
