2b4e6a002986a184fb55856e7e64b6289c17d7bd
Youssef El Khoury <usf.kh96@gmail.com>
2017-05-04 21:31:59 +0300

    test

--- donation_center/dashboard_dc.php
+++ donation_center/dashboard_dc.php
@@ -285,12 +285,47 @@
 				},
 				{
 					"targets": -2,
-					"className": 'details-control',
-					"data": null,
-					"defaultContent": "<button type='button'>Delete</button>"
+					"className":      'details-control',
+					"orderable":      false,
+					"data":           null,
+					"defaultContent": ''
 				}
 				]
 			});
+			
+			function format ( d ) {
+				// `d` is the original data object for the row
+				return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+					'<tr>'+
+						'<td>Full name:</td>'+
+						'<td>'+d.name+'</td>'+
+					'</tr>'+
+					'<tr>'+
+						'<td>Extension number:</td>'+
+						'<td>'+d.extn+'</td>'+
+					'</tr>'+
+					'<tr>'+
+						'<td>Extra info:</td>'+
+						'<td>And any further details here (images etc)...</td>'+
+					'</tr>'+
+				'</table>';
+			}
+			
+			$('#dataTables-bloodtype tbody').on('click', 'td.details-control', function () {
+				var tr = $(this).closest('tr');
+				var row = table.row( tr );
+		 
+				if ( row.child.isShown() ) {
+					// This row is already open - close it
+					row.child.hide();
+					tr.removeClass('shown');
+				}
+				else {
+					// Open this row
+					row.child( format(row.data()) ).show();
+					tr.addClass('shown');
+				}
+			} );
 			
 			$('#dataTables-bloodtype tbody').on( 'click', 'button', function () {
 				var delrow = dataTables_bloodtype.row( $(this).parents('tr') );
